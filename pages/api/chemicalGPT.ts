902691fbbbaf9bafea3594b665126bf728a68e6f
JWest11 <114183436+JWest11@users.noreply.github.com>
2023-04-17 01:18:38 -0600

    updated api

--- pages/api/chemicalGPT.ts
+++ pages/api/chemicalGPT.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
-    apiKey: process.env.JWestOpenAiAPIKey
+    apiKey: process.env.OPEN_AI_API_KEY
 })
 
 const openai = new OpenAIApi(configuration);
